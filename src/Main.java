1c2e3fb54bd5d30e403a033913b3fdd0511a2beb
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2023-10-24 16:28:31 -0300

    codigos de formatacao

--- src/Main.java
+++ src/Main.java
@@ -2,29 +2,7 @@ import java.util.Scanner;
 
 
 public class Main {
-
-//    # Exemplo em Python
-//    numero = int(input("Digite um número: "))
-//
-//            if numero > 0:
-//    print("O número é positivo.")
-//    elif numero < 0:
-//    print("O número é negativo.")
-//    else:
-//    print("O número é zero.")
-
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite um número: ");
-
-        int numero = scanner.nextInt();
-
-        if (numero > 0) {
-            System.out.println("O número é positivo.");
-        } else if (numero < 0) {
-            System.out.println("O número é negativo.");
-        } else {
-            System.out.println("O número é zero.");
-        }
+        System.out.println("linha1\tcoluna1\nlinha2\tcoluna2");
     }
 }
