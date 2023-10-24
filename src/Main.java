c229a154a0453485c98e2f09731538aba1c7adce
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2023-10-24 19:51:49 -0300

    Exercicios/ Segundo

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,18 @@
 import java.util.Scanner;
 
-//1. Crie um algoritmo em Java que solicita três números decimais ao usuário.
-// Em seguida, a média destes números é calculada e mostrada na tela para o usuário.
+//2. Crie um algoritmo em Java que verifica se um número inteiro é positivo, negativo ou zero.
 public class Main {
     public static void main(String[] args) {
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("1- Insira 1 numero de 0 a 10");
+        System.out.println("Insira 1 numero");
         int input1 = scanner.nextInt();
 
-        System.out.println("2- Insira 1 numero de 0 a 10");
-        int input2 = scanner.nextInt();
-
-        System.out.println("3- Insira 1 numero de 0 a 10");
-        int input3 = scanner.nextInt();
-
-        System.out.println((input1 + input2 + input3) / 3);
-
+        if (input1 > 0) {
+            System.out.println("positivo");
+        } else if (input1 < 0) {
+            System.out.println("negativo");
+        } else {
+            System.out.println("zero");
+        }
     }
 }
