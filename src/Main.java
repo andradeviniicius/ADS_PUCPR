2c79ebdd7b4c956860c4c1ea9108d81b690d7b80
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2023-10-24 16:50:11 -0300

    utilizando loops

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,25 @@
-//EXEMPLOExemplo de aplicação 6: crie um algoritmo em Java o qual informe ao usuário se um determinado número é igual a 1, igual a 2, igual a 3, ou se o número é diferente de 1, 2 e 3.
+//EXEMPLOExemplo de aplicação 7: converta o código a seguir de Python para Java.
 
 import java.util.Scanner;
 
 public class Main {
+    /*
+    *   contador = 1
+        while contador <= 5:
+        print("Contagem:", contador)
+        contador += 1
+    * */
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite um numero: ");
-        int numero = scanner.nextInt();
+        int contador = scanner.nextInt();
 
-        switch (numero) {
-            case 1:
-                System.out.println("O número é igual a 1");
-                break;
-            case 2:
-                System.out.println("O número é igual a 2");
-                break;
-            case 3:
-                System.out.println("O número é igual a 3");
-                break;
-            default:
-                System.out.println("O número é diferente de 1, 2 e 3");
-                break;
+        while (contador <= 5) {
+            System.out.println("Contagem: " + contador);
+            contador++;
         }
+
     }
 }
 
