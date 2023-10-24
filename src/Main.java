36cde6fcf1753a75f187fa96968b1a95856d035a
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2023-10-24 16:29:43 -0300

    constantes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
-import java.util.Scanner;
-
-
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("linha1\tcoluna1\nlinha2\tcoluna2");
+
+        // Exemplos de declaração e atribuição de variável
+
+        int numero;
+
+        float altura = 1.70f;
+
+        String nome;
+
+
+        // Exemplos de declaração e atribuição de constante
+
+        final float PI = 3.1416F;
+
+        final String NOME_PAGINA = "home";
+
     }
+
 }
