d66efc611d48d202242b8f662451ed04c2546976
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2023-10-24 16:53:22 -0300

    utilizando loops for break e continue

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,28 @@
-//EXEMPLOExemplo de aplicação 9: converta o código a seguir de Python para Java.
-
-import java.util.Scanner;
-
+//Exemplo de aplicação 10: crie dois loops usando Java. O primeiro deve usar “break”, e o segundo, “continue”.
 public class Main {
-    /*
-    *   for i in range(10):
-        print("Número:", i)
-    * */
+
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
+        // Exemplo de uso do break
+
+        System.out.println("Exemplo com break:");
+
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                break; // Encerra o loop quando i == 3
+            }
+            System.out.println("Número: " + i);
+        }
+        System.out.println();
+
+        // Exemplo de uso do continue
+
+        System.out.println("Exemplo com continue:");
+
+        for (int i = 1; i <= 5; i++) {
+            if (i == 3) {
+                continue; // Pula para a próxima iteração quando i == 3
+            }
             System.out.println("Número: " + i);
         }
     }
 }
-
