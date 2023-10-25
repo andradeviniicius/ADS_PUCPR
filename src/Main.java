390f8ced071c2fa3497767d5e235637bef292a2d
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2023-10-24 21:20:32 -0300

    Exercicios/ Sexto

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,34 @@
 import java.util.Scanner;
 
-//5. Crie um algoritmo em Java que peça ao usuário para que se digite um ano (exemplo: 2032). Em seguida, o algoritmo deve verificar se o ano informado é ou não bissexto.
+//6. Crie um algoritmo em Java que solicita um número inteiro ao usuário. Em seguida, deve-se verificar se o número digitado é ou não é um número primo.
 public class Main {
     public static void main(String[] args) {
 
+        
+        // Números primos são números que têm apenas 2 fatores: 1 e ele mesmo. Por exemplo, os 5 primeiros números primos são 2, 3, 5, 7 e 11.
+
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Escolha um ano");
+        System.out.println("Escolha um numero");
         int input = scanner.nextInt();
+        boolean primo = true;
 
-        if ((input % 4 == 0 && input % 100 != 0) || input % 400 == 0) {
-            System.out.println("Ano bissexto");
+
+        if (input <= 1) {
+            primo = false;
         } else {
-            System.out.println("Ano normal");
-
+            for (int i = 2; i <= Math.sqrt(input); i++) {
+                if (input % i == 0) {
+                    primo = false;
+                    break;
+                }
+            }
         }
+
+        if (primo) {
+            System.out.println("O número é primo.");
+        } else {
+            System.out.println("O número não é primo.");
+        }
+
     }
 }
