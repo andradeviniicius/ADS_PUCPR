3683cd3406c5d14fa4870bc730ecc1b754a1f2f7
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2023-10-25 14:04:03 -0300

    Exercicios/ Oitavo Outra maneira

--- src/Main.java
+++ src/Main.java
@@ -7,17 +7,20 @@ import java.util.Scanner;
 //****
 //*****
 public class Main {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Escolha um numero");
+        System.out.print("Digite um número inteiro: ");
 
-        int input = scanner.nextInt();
-        String drawing = "*";
+        int n = scanner.nextInt();
 
-        for (int i = 1; i <= input; i++) {
-            System.out.println(drawing);
-            drawing += "*";
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
 
+        scanner.close();
     }
 }
