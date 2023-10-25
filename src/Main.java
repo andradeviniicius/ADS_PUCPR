0b98766bf5e8af2f9740e59422cb45d04fab13f8
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2023-10-25 14:03:42 -0300

    Exercicios/ Oitavo

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,23 @@
 import java.util.Scanner;
 
-//7. Crie um algoritmo em Java que peça ao usuário a temperatura em graus Celsius e converta-a para graus Fahrenheit. Além disso, o programa deve imprimir se está frio (abaixo de 32°F), moderado (entre 32°F e 80°F) ou quente (acima de 80°F).
+//8. Desenvolva um algoritmo em Java que peça ao usuário um número inteiro n e gere um padrão de asteriscos. Por exemplo, se n for 5, o padrão seria assim:
+//*
+//**
+//***
+//****
+//*****
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Escolha uma temp em celsius");
-        if (scanner.hasNextDouble()) {
-            double tempCelsius = scanner.nextInt();
-            double tempFahrenheit = 0;
+        System.out.println("Escolha um numero");
 
-            //Fahrenheit (°F) = (Temperature in degrees Celsius (°C) * 9/5) + 32
+        int input = scanner.nextInt();
+        String drawing = "*";
 
-            tempFahrenheit = (tempCelsius * 9 / 5) + 32;
-
-
-            if (tempFahrenheit > 80) {
-                System.out.println("Está quente");
-            } else if (tempFahrenheit < 32) {
-                System.out.println("Está frio");
-            } else {
-                System.out.println("Está normal");
-            }
-        } else {
-            System.out.println("Invalid input. Please enter a numeric value for temperature.");
+        for (int i = 1; i <= input; i++) {
+            System.out.println(drawing);
+            drawing += "*";
         }
+
     }
 }
