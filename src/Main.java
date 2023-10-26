a00be53cdcfd518c98bd335feb58fbd28259b8e5
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2023-10-26 18:33:54 -0300

    Exercicios/ Nono

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-//8. Desenvolva um algoritmo em Java que peça ao usuário um número inteiro n e gere um padrão de asteriscos. Por exemplo, se n for 5, o padrão seria assim:
+//9. Crie um algoritmo em Java que solicita ao usuário para que insira um número inteiro entre 1 e 1000 e converta-o em um número romano. Por exemplo, se o usuário inserir 58, o programa deve imprimir "LVIII".
 //*
 //**
 //***
@@ -12,15 +12,39 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Digite um número inteiro: ");
 
-        int n = scanner.nextInt();
+        int input = scanner.nextInt();
+        String valorRomano = "";
+        int valorNumerico = input;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+        
+
+        String[][] romanos = {
+                {"M", "1000"},
+                {"CM", "900"},
+                {"D", "500"},
+                {"CD", "400"},
+                {"C", "100"},
+                {"XC", "90"},
+                {"L", "50"},
+                {"XL", "40"},
+                {"X", "10"},
+                {"IX", "9"},
+                {"V", "5"},
+                {"IV", "4"},
+                {"I", "1"}
+        };
+
+        //150
+
+        for (int i = 0; i <= romanos.length - 1; i++) {
+            if (valorNumerico >= Integer.parseInt(romanos[i][1])) {
+                valorNumerico -= Integer.parseInt(romanos[i][1]);
+                valorRomano += romanos[i][0];
             }
-            System.out.println();
         }
 
-        scanner.close();
+        System.out.println(valorNumerico);
+        System.out.println(valorRomano);
+
     }
 }
