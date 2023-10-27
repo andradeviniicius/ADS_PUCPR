a0aa5507232759a9afed226c0563b52f4f49e881
Vinicius de Andrade <viniciusdandrade01@gmail.com>
2023-10-27 14:22:26 -0300

    AF1/ Inicio projeto da disciplina

---
+++ src/Financiamento.java
@@ -0,0 +1,15 @@
+//Classe Financiamento:
+//i. Esta classe representará um financiamento.
+//
+//ii. Ela deve conter três atributos: valorImovel (double), prazoFinanciamento (int) e taxaJurosAnual (double).
+//
+//iii. Deve conter pelo menos três métodos: um construtor para inicializar esses atributos, um método para calcular o pagamento mensal, e outro método para calcular o total do pagamento.
+//
+//iv. As fórmulas para calcular o pagamento mensal e total do pagamento serão:
+//
+//1. Pagamento mensal = (valor do imóvel / prazo do financiamento) * (1 + (taxa anual / 12))
+//
+//2. Total do pagamento = pagamento mensal * prazo do financiamento
+
+public class Financiamento {
+}
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,16 @@
+//c. Classe Main:
+//
+//i. Esta é a classe principal do programa.
+//
+//ii. Ela deve conter o método main(), onde o fluxo principal do programa será implementado.
+//
+//1. Dentro do método main() vocêdeve usar os métodos da classe InterfaceUsuario para ler os dados do financiamento.
+//
+//2. Após ler os dados do financiamento, instancie um objeto do tipo Financiamento para criar este financiamento.
+
 public class Main {
 
     public static void main(String[] args) {
 
-        int x = 20;
-        int y = 10;
-
-
-        System.out.println(Calculadora.sqrt(x));
     }
 }
